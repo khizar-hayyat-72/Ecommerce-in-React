1326ce91afa00b6d56124e05eeecd96427fe880d
Khizar Hayyat Jafar <Khizarhayyt199093@gmail.com>
2021-09-12 10:37:02 +0500

    Create product.jsx

---
+++ ecom-app/src/components/Products/Product/product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Card, CardMedia, CardContent,CardAction, Typography, IconButton } from '@material-ui/core'
+
+const product = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default product
