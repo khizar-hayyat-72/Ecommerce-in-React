4a7a8789f3d4f58d726dde6336052f5fc1bf497a
Khizar Hayyat Jafar <Khizarhayyt199093@gmail.com>
2021-09-13 11:25:53 +0500

    Styles

--- ecom-app/src/components/Products/Product/product.jsx
+++ ecom-app/src/components/Products/Product/product.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
-import {Card, CardMedia, CardContent,CardAction, Typography, IconButton } from '@material-ui/core'
+import {Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
+import {AddShoppingCart} from '@material-ui/icons'
+import useStyles from './styles';
 
-const product = () => {
+
+const Product = ({product}) => {
+    const classes = useStyles();
     return (
-        <div>
-            
-        </div>
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image= '' title={product.name} />
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant='h5'  gutterBottom >
+                        {product.name}
+                    </Typography>
+                    <Typography variant='h5'>
+                        {product.price}
+                    </Typography>
+                </div>
+                <Typography variant='body2' color='textSecondary'>
+                    {product.description}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing className={classes.CardAction}>
+                <IconButton aria-label = 'Add to Cart'>
+                    <AddShoppingCart/>
+                </IconButton>
+            </CardActions>   
+        </Card>
     )
 }
 
-export default product
+export default Product
